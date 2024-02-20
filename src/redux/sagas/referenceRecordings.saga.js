ac3ca4552b09bd833422c4a64ced3c8b077cd062
Andrew Haarsager <athaarsager@gmail.com>
2024-02-19 21:05:30 -0600

    Create fetchReferenceRecordings saga

--- src/redux/sagas/referenceRecordings.saga.js
+++ src/redux/sagas/referenceRecordings.saga.js
@@ -1,8 +1,18 @@
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
-function* referenceRecordingsSaga() {
+function* fetchReferenceRecordings(action) {
+    try {
+    const recordingsResponse = yield axios.get(`/api/reference_recordings/${action.payload}`);
+    yield put({ type: "SET_RECORDINGS", payload: recordingsResponse.data});
 
+    } catch(error) {
+        console.error("ERROR in fetchReferenceRecordings saga:", error);
+    }
+}
+
+function* referenceRecordingsSaga() {
+    yield takeLatest("FETCH_REFERENCE_RECORDINGS", fetchReferenceRecordings);
 }
 
 export default referenceRecordingsSaga;
