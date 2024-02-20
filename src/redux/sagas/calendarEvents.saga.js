d2b0d77ed99de126e521e02c4cdb79db39777038
Andrew Haarsager <athaarsager@gmail.com>
2024-02-20 13:55:50 -0600

    Create fetchCalendarEvents saga

--- src/redux/sagas/calendarEvents.saga.js
+++ src/redux/sagas/calendarEvents.saga.js
@@ -1,8 +1,17 @@
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
-function* calendarEventsSaga() {
+function* fetchCalendarEvents() {
+    try {
+        const fetchCalendarResponse = yield axios.get("/api/calendar_events");
+        yield put({ type:"SET_CALENDAR_EVENTS", payload: fetchCalendarResponse});
+    } catch(error) {
+        console.error("ERROR in fetchCalendarEvents saga:", error);
+    }
+}
 
+function* calendarEventsSaga() {
+    yield takeLatest("FETCH_CALENDAR_EVENTS", fetchCalendarEvents);
 }
 
 export default calendarEventsSaga;
