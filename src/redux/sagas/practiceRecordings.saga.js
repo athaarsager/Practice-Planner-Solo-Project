cf3adf7d2cde90619026c156d2b092da6a4ffc9b
Andrew Haarsager <athaarsager@gmail.com>
2024-02-20 10:43:03 -0600

    Create addNewPracticeRecording saga

--- src/redux/sagas/practiceRecordings.saga.js
+++ src/redux/sagas/practiceRecordings.saga.js
@@ -21,9 +21,22 @@ function* fetchLatestPracticeRecording(action) {
     }
 }
 
+function* addNewPracticeRecording(action) {
+    // action.payload needs to include file name and plan id 
+    try {
+        yield axios.post("/api/practice_recordings/upload_file", action.payload);
+        // don't need to yield a fetch request since recording won't be viewable until
+        // user looks at plan in edit mode, or sees it as the most recent recording
+        // on main piece page
+    } catch(error) {
+        console.error("ERROR in addNewPracticeRecording saga:", error);
+    }
+}
+
 function* practiceRecordingsSaga() {
     yield takeLatest("FETCH_SELECTED_PRACTICE_RECORDING", fetchSelectedPracticeRecording);
     yield takeLatest("FETCH_LATEST_PRACTICE_RECORDING", fetchLatestPracticeRecording);
+    yield takeLatest("ADD_NEW_PRACTICE_RECORDING", addNewPracticeRecording);
 }
 
 export default practiceRecordingsSaga;
