c8bc7538eaf394ae512bc70b54357ed39ba9743e
Andrew Haarsager <athaarsager@gmail.com>
2024-02-19 19:22:57 -0600

    Create fetchPlans saga

--- src/redux/sagas/practicePlans.saga.js
+++ src/redux/sagas/practicePlans.saga.js
@@ -1,8 +1,20 @@
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
-function* practicePlansSaga() {
 
+function* fetchPlans() {
+    try {
+        const plansResponse = yield axios.get("/api/practice_plans");
+        yield put({ type: "SET_PLANS", payload: plansResponse.data});
+    } catch (error) {
+        console.error("ERROR in fetchPlans saga:", error);
+    }
+}
+
+
+
+function* practicePlansSaga() {
+    yield takeLatest("FETCH_PLANS", fetchPlans)
 }
 
 export default practicePlansSaga;
