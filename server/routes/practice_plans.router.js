cc506cdda9ca4402be4d280f3c20f7e3dab64ad8
Andrew Haarsager <athaarsager@gmail.com>
2024-02-19 19:35:47 -0600

    Single GET route works

--- server/routes/practice_plans.router.js
+++ server/routes/practice_plans.router.js
@@ -20,6 +20,24 @@ router.get("/", rejectUnauthenticated, (req, res) => {
         });
 });
 
+// GET SINGLE plan for detail view/editing
+router.get("/:id", rejectUnauthenticated, (req, res) => {
+    const planId = req.params.id;
+    const queryText = `
+    SELECT "practice_plans"."section", "practice_plans"."problems", "practice_plans"."plan", "practice_plans"."goal" FROM "practice_plans"
+    JOIN "pieces" ON "pieces"."id" = "practice_plans"."piece_id"
+    WHERE "pieces"."user_id" = $1 AND "practice_plans"."id" = $2; 
+    `;
+    pool.query(queryText, [req.user.id, planId])
+    .then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log("ERROR in SINGLE practice_plan GET:", error);
+        res.sendStatus(500);
+    });
+});
+
+
 // POST new practice plan
 router.post("/", rejectUnauthenticated, (req, res) => {
     // Need to make sure to send the piece id in req.body
