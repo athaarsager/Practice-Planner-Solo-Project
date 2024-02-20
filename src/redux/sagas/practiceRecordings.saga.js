0dfc46e5179ec7ba5da6fe993dd76cf16a11147b
Andrew Haarsager <athaarsager@gmail.com>
2024-02-20 10:55:42 -0600

    Create deletePracticeRecording saga

--- src/redux/sagas/practiceRecordings.saga.js
+++ src/redux/sagas/practiceRecordings.saga.js
@@ -26,10 +26,23 @@ function* addNewPracticeRecording(action) {
     try {
         yield axios.post("/api/practice_recordings/upload_file", action.payload);
         // don't need to yield a fetch request since recording won't be viewable until
-        // user looks at plan in edit mode, or sees it as the most recent recording
+        // user reviews reflection page, or sees it as the most recent recording
         // on main piece page
     } catch(error) {
         console.error("ERROR in addNewPracticeRecording saga:", error);
+    }
+}
+
+function* deletePracticeRecording(action) {
+    // need recording id in action.payload
+    try {
+        yield axios.delete(`/api/practice_recordings/delete/${action.payload}`);
+        // Executive decision: user can only delete from reflection page. When most recent recording is displayed
+        // on practice entries screen, user will be able to press a button to view the related reflection
+        yield put({ type: "SET_SELECTED_PRACTICE_RECORDING", payload: 0});
+        // sending payload of 0 because the recording id can never equal 0. query will go through and return nothing
+    } catch(error) {
+        console.error("ERROR in deletePracticeRecording saga:", error);
     }
 }
 
@@ -37,6 +50,7 @@ function* practiceRecordingsSaga() {
     yield takeLatest("FETCH_SELECTED_PRACTICE_RECORDING", fetchSelectedPracticeRecording);
     yield takeLatest("FETCH_LATEST_PRACTICE_RECORDING", fetchLatestPracticeRecording);
     yield takeLatest("ADD_NEW_PRACTICE_RECORDING", addNewPracticeRecording);
+    yield takeLatest("DELETE_PRACTICE_RECORDING", deletePracticeRecording);
 }
 
 export default practiceRecordingsSaga;
