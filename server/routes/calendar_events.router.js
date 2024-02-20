0dbbbcf0f01a844191fefa4a3db5c542bf75e247
Andrew Haarsager <athaarsager@gmail.com>
2024-02-20 13:22:58 -0600

    Create calendar_events GET. Tested with Postman. Works!

--- server/routes/calendar_events.router.js
+++ server/routes/calendar_events.router.js
@@ -5,4 +5,19 @@ const {
     rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
 
+// GET all calendar events
+router.get("/", rejectUnauthenticated, (req, res) => {
+    const queryText = `
+    SELECT "calendar_events"."title", "calendar_events"."date", "calendar_events"."start", "calendar_events"."end"
+    FROM "calendar_events"
+    WHERE "user_id" = $1;
+    `;
+    pool.query(queryText, [req.user.id])
+        .then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log("ERROR in calendar_events GET:", error);
+        });
+});
+
 module.exports = router;
