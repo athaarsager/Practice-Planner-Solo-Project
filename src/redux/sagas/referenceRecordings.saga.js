1d5a6684c8de28087ca72df950f7b819bfaa78f4
Andrew Haarsager <athaarsager@gmail.com>
2024-02-19 21:17:13 -0600

    Create deleteReferenceRecording saga, fix typos

--- src/redux/sagas/referenceRecordings.saga.js
+++ src/redux/sagas/referenceRecordings.saga.js
@@ -4,7 +4,7 @@ import axios from "axios";
 function* fetchReferenceRecordings(action) {
     try {
     const recordingsResponse = yield axios.get(`/api/reference_recordings/${action.payload}`);
-    yield put({ type: "SET_RECORDINGS", payload: recordingsResponse.data});
+    yield put({ type: "SET_RECORDINGS", payload: recordingsResponse.data });
 
     } catch(error) {
         console.error("ERROR in fetchReferenceRecordings saga:", error);
@@ -14,7 +14,7 @@ function* fetchReferenceRecordings(action) {
 function* fetchSelectedRecording(action) {
     try {
         const recordingResponse = yield axios.get(`/api/reference_recordings/${action.payload}`);
-        yield put({ type: "SET_SELECTED_RECORDING", payload: recordingResponse.data});
+        yield put({ type: "SET_SELECTED_RECORDING", payload: recordingResponse.data });
 
     } catch(error) {
         console.error("ERROR in fetchSelectedRecording saga:", error);
@@ -24,7 +24,7 @@ function* fetchSelectedRecording(action) {
 function* addReferenceRecording(action) {
     try {
         yield axios.post("/api/reference_recordings/", action.payload);
-        yield put({ type: "FETCH_REFERENCE_RECORDINGS"});
+        yield put({ type: "FETCH_REFERENCE_RECORDINGS" });
     } catch(error) {
         console.error("ERROR in addReferenceRecording saga:", error);
     }
@@ -33,17 +33,27 @@ function* addReferenceRecording(action) {
 function* editReferenceRecording(action) {
     try {
         yield axios.put(`/api/reference_recordings/${action.payload.id}`, action.payload);
-        yield put({ type: "FETCH_SELECTED_RECORDING"});
+        yield put({ type: "FETCH_SELECTED_RECORDING" });
     } catch(error) {
         console.error("ERROR in editReferenceRecording saga:", error);
+    }
+}
+
+function* deleteReferenceRecording(action) {
+    try {
+        yield axios.delete(`/api/reference_recordings/${action.payload}`);
+        yield put({ type: "FETCH_REFERENCE_RECORDINGS" });
+    } catch(error) {
+        console.error("ERROR in deleteReferenceRecording saga:", error);
     }
 }
 
 function* referenceRecordingsSaga() {
     yield takeLatest("FETCH_REFERENCE_RECORDINGS", fetchReferenceRecordings);
     yield takeLatest("FETCH_SELECTED_RECORDING", fetchSelectedRecording);
-    yield takeLatest("ADD_REFERENCE_RECORDING");
-    yield takeLatest("EDIT_REFERENCE_RECORDING");
+    yield takeLatest("ADD_REFERENCE_RECORDING", addReferenceRecording);
+    yield takeLatest("EDIT_REFERENCE_RECORDING", editReferenceRecording);
+    yield takeLatest("DELETE_REFERENCE_RECORDING", deleteReferenceRecording);
 }
 
 export default referenceRecordingsSaga;
