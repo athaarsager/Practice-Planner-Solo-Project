03d2127cf11fcbc193157bf711b90146f9443081
Andrew Haarsager <athaarsager@gmail.com>
2024-02-19 19:38:14 -0600

    Create fetchSinglePlan saga

--- src/redux/sagas/practicePlans.saga.js
+++ src/redux/sagas/practicePlans.saga.js
@@ -11,6 +11,15 @@ function* fetchPlans() {
     }
 }
 
+function* fetchSinglePlan(action) {
+    try {
+        const planResponse = yield axios.get(`/api/practice_plans/${action.payload}`);
+        yield put({ type: "SET_SINGLE_PLAN", payload: planResponse});
+    } catch(error) {    
+        console.error("ERROR in fetchSinglePlan saga:", error);
+    }
+}
+
 function* addPlan(action) {
     try {
         yield axios.post("/api/practice_plans", action.payload);
@@ -41,6 +50,7 @@ function deletePlan(action) {
 
 function* practicePlansSaga() {
     yield takeLatest("FETCH_PLANS", fetchPlans);
+    yield takeLatest("FETCH_SINGLE_PLAN", fetchSinglePlan);
     yield takeLatest("ADD_PLAN", addPlan);
     yield takeLatest("EDIT_PLAN", editPlan);
     yield takeLatest("DELETE_PLAN", deletePlan);
