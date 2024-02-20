28fbcc6d6c73380fce66d0d1303d5ab7df699d7a
Andrew Haarsager <athaarsager@gmail.com>
2024-02-19 19:14:19 -0600

    Create practice_plans DELETE route. Tested with Postman. Works!

--- server/routes/practice_plans.router.js
+++ server/routes/practice_plans.router.js
@@ -3,25 +3,25 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const {
     rejectUnauthenticated,
-  } = require('../modules/authentication-middleware');
+} = require('../modules/authentication-middleware');
 
-  // GET practice plans from database
-  router.get("/", rejectUnauthenticated, (req, res) => {
+// GET practice plans from database
+router.get("/", rejectUnauthenticated, (req, res) => {
     const queryText = `
     SELECT "practice_plans"."section", "practice_plans"."problems", "practice_plans"."plan", "practice_plans"."goal" FROM "practice_plans"
     JOIN "pieces" ON "pieces"."id" = "practice_plans"."piece_id"
-    WHERE "pieces"."user_id" = $1; `; 
+    WHERE "pieces"."user_id" = $1; `;
     pool.query(queryText, [req.user.id])
-    .then((result) => {
-        res.send(result.rows);
-    }).catch((error) => {
-        console.log("ERROR in practice_plans GET:", error);
-        res.sendStatus(500);
-    });
-  });
+        .then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log("ERROR in practice_plans GET:", error);
+            res.sendStatus(500);
+        });
+});
 
-  // POST new practice plan
-  router.post("/", rejectUnauthenticated, (req, res) => {
+// POST new practice plan
+router.post("/", rejectUnauthenticated, (req, res) => {
     // Need to make sure to send the piece id in req.body
     const piece = req.body;
     const queryText = `
@@ -29,28 +29,40 @@ const {
     VALUES ($1, $2, $3, $4, $5);
     `;
     pool.query(queryText, [piece.piece_id, piece.section, piece.problems, piece.plan, piece.goal])
-    .then((result) => {
-        res.sendStatus(201);
-    }).catch((error) => {
-        console.log("ERROR in practice_plans POST:", error);
-    });
-  });
+        .then((result) => {
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log("ERROR in practice_plans POST:", error);
+        });
+});
 
-  // PUT (edit) existing plan
-  router.put("/:id", rejectUnauthenticated, (req, res) => {
+// PUT (edit) existing plan
+router.put("/:id", rejectUnauthenticated, (req, res) => {
     const planId = req.params.id;
     const piece = req.body;
-    console.log(planId);
     const queryText = `
     UPDATE "practice_plans" SET "section" = $1, "problems" = $2, "plan" = $3, "goal" = $4
     WHERE "piece_id" = $5;  
     `;
     pool.query(queryText, [piece.section, piece.problems, piece.plan, piece.goal, planId])
-    .then((result) => {
-        res.sendStatus(200);
-    }).catch((error) => {
-        console.log("ERROR in practice_plans PUT:", error);
-    });
-  });
+        .then((result) => {
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log("ERROR in practice_plans PUT:", error);
+        });
+});
 
-  module.exports = router;
+router.delete("/:id", rejectUnauthenticated, (req, res) => {
+    const planId = req.params.id;
+    const queryText = `
+    DELETE FROM "practice_plans" WHERE "id" = $1;
+    `;
+    pool.query(queryText, [planId])
+        .then((result) => {
+            res.sendStatus(204);
+        }).catch((error) => {
+            console.log("ERROR in practice_plans DELETE:", error);
+        });
+});
+
+module.exports = router;
