49b3242bd6349ed4965a1428c5c6703e2372cf41
Andrew Haarsager <athaarsager@gmail.com>
2024-02-19 18:32:08 -0600

    GET server route works for practice_plans

--- server/routes/practice_plans.router.js
+++ server/routes/practice_plans.router.js
@@ -7,7 +7,17 @@ const {
 
   // GET practice plans from database
   router.get("/", rejectUnauthenticated, (req, res) => {
-    const queryText = `SELECT * FROM "practice_plans" WHERE `
-  })
+    const queryText = `
+    SELECT "practice_plans"."section", "practice_plans"."problems", "practice_plans"."plan", "practice_plans"."goal" FROM "practice_plans"
+    JOIN "pieces" ON "pieces"."id" = "practice_plans"."piece_id"
+    WHERE "pieces"."user_id" = $1 `; 
+    pool.query(queryText, [req.user.id])
+    .then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log("ERROR in practice_plans GET:", error);
+        res.sendStatus(500);
+    });
+  });
 
   module.exports = router;
