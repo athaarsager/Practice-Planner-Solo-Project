f322738879f7a8c005294c437836f747156bd65b
Andrew Haarsager <athaarsager@gmail.com>
2024-02-20 09:24:54 -0600

    Create GET route for single recording. Tested with Postman. Works!

--- server/routes/practice_recordings.router.js
+++ server/routes/practice_recordings.router.js
@@ -5,4 +5,19 @@ const {
     rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
 
+// GET for single practice recording attached to plan
+router.get("/:id", rejectUnauthenticated, (req, res) => {
+    const recordingId = req.params.id;
+    const queryText = `
+    SELECT "file_name" FROM "practice_recordings" WHERE "id" = $1;
+    `;
+    pool.query(queryText, [recordingId])
+    .then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log("ERROR in single practice_recording GET:", error);
+        res.sendStatus(500);
+    });
+});
+
 module.exports = router;
