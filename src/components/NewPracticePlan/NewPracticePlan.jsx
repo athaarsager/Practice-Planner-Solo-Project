798dfcbfb069be1b61ae2dc6346ef0fe969fb759
Andrew Haarsager <athaarsager@gmail.com>
2024-02-21 17:10:35 -0600

    Update size of inputs for now

--- src/components/NewPracticePlan/NewPracticePlan.jsx
+++ src/components/NewPracticePlan/NewPracticePlan.jsx
@@ -7,9 +7,16 @@ function NewPracticePlan() {
     return (
         <>
         <h1>New Practice Plan</h1>
+        {/* Use css to inrease size of inputs. Also see what MUI provides */}
         <form>
-            <label htmlFor="section">What section are you working on?</label>
-            <input id="section" name="section" type="text" placeholder="Your Answer Here" />
+            <label htmlFor="section">What section are you working on?</label><br/>
+            <input id="section" name="section" type="text" placeholder="Your Answer Here" size="100" /><br/>
+            <label htmlFor="problems">What are the problems you need to solve/issues you need to address in this section?</label><br/>
+            <input id="problems" name="problems" type="text" placeholder="Your Answer Here" size="100" /><br/>
+            <label htmlFor="plan">How will you solve these problems/address these issues?</label><br/>
+            <input id="plan" name="plan" type="text" placeholder="Your Answer Here" size="100" /><br/>
+            <label htmlFor="goal">What is your goal for the end of the practice session?</label><br/>
+            <input id="goal" name="goal" type="text" placeholder="Your Answer Here" size="100" /><br/>
         </form>
         </>
     );
