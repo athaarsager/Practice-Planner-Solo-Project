9ffe205d2096fffceca5ca4b463b7c5478888a75
Andrew Haarsager <athaarsager@gmail.com>
2024-02-21 19:38:57 -0600

    Submit for new practice plan works!

--- src/components/NewPracticePlan/NewPracticePlan.jsx
+++ src/components/NewPracticePlan/NewPracticePlan.jsx
@@ -1,25 +1,48 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useState } from "react";
 
 function NewPracticePlan() {
     const dispatch = useDispatch();
     const history = useHistory();
+    const pieceId = useParams().id;
+
+    const [responses, setResponses] = useState(
+        {
+            piece_id: pieceId,
+            section: "",
+            problems: "",
+            plan: "",
+            goal: ""
+        });
+
+        const handleChange = (e) => {
+            const { name, value } = e.target;
+
+            setResponses((state) => ({...state, [name]: value}));
+        }
+
+    const submitPlan = (e) => {
+        e.preventDefault();
+        dispatch({ type: "ADD_PLAN", payload: responses});
+        history.goBack();
+    }
+
     return (
         <>
             <h1>New Practice Plan</h1>
             {/* Use css to inrease size of inputs. Also see what MUI provides */}
-            <form>
+            <form onSubmit={submitPlan}>
                 <label htmlFor="section">What section are you working on?</label><br />
-                <input id="section" name="section" type="text" placeholder="Your Answer Here" size="100" /><br />
+                <input id="section" name="section" type="text" placeholder="Your Answer Here" size="100" value={responses.section} onChange={handleChange} /><br />
                 <label htmlFor="problems">What are the problems you need to solve/issues you need to address in this section?</label><br />
-                <input id="problems" name="problems" type="text" placeholder="Your Answer Here" size="100" /><br />
+                <input id="problems" name="problems" type="text" placeholder="Your Answer Here" size="100" value={responses.problems} onChange={handleChange} /><br />
                 <label htmlFor="plan">How will you solve these problems/address these issues?</label><br />
-                <input id="plan" name="plan" type="text" placeholder="Your Answer Here" size="100" /><br />
-                <label htmlFor="goal">What is your goal for the end of the practice session?</label><br />
-                <input id="goal" name="goal" type="text" placeholder="Your Answer Here" size="100" /><br />
+                <input id="plan" name="plan" type="text" placeholder="Your Answer Here" size="100" value={responses.plan} onChange={handleChange} /><br />
+                <label htmlFor="goal">What is your goal for the end of the practice session? e.g. runs without mistakes, target metronome marking, etc.</label><br />
+                <input id="goal" name="goal" type="text" placeholder="Your Answer Here" size="100" value={responses.goal} onChange={handleChange} /><br />
                 <div>
                     <p>Create Calendar Event? Optional</p>
-                    {/* Do I need two buttons? Probably only need yes... */}
                     <button type="button">Yes</button>
                 </div>
                 <button type="button">Cancel</button>
