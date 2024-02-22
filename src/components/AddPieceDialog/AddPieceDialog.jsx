c95671273be4aaaeaad431aa08daf6bb0c87f13d
Andrew Haarsager <athaarsager@gmail.com>
2024-02-22 11:05:59 -0600

    Create local state for newPiece form

--- src/components/AddPieceDialog/AddPieceDialog.jsx
+++ src/components/AddPieceDialog/AddPieceDialog.jsx
@@ -1,12 +1,19 @@
-function AddPieceDialog() {
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+function AddPieceDialog({open, closeNewPiece}) {
+    const dispatch = useDispatch();
+
+    const [pieceTitle, setPieceTitle] = useState("");
+    const [pieceComposer, setPieceComposer] = useState("");
+
     return (
         <>
-        <dialog>
+        <dialog open={open} close={closeNewPiece}>
             <form>
                 <label htmlFor="title">Title</label><br />
-                <input id="title" name="title" type="text" placeholder="Title Here" /><br />
+                <input id="title" name="title" type="text" placeholder="Title Here" value={pieceTitle} onChange={(e) => setPieceTitle(e.target.value)} /><br />
                 <label htmlFor="composer">Composer</label><br />
-                <input id="composer" name="composer" type="text" placeholder="Composer Name" />
+                <input id="composer" name="composer" type="text" placeholder="Composer Name" value={pieceComposer} onChange={(e) => setPieceComposer(e.target.value)} />
             </form>
         </dialog>
         </>
