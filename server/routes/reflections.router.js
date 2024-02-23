08a74f109da500ad35843ef7506477d6c36f48d4
Andrew Haarsager <athaarsager@gmail.com>
2024-02-22 20:30:32 -0600

    Create reflections POST route. Tested with Postman. Works!

--- server/routes/reflections.router.js
+++ server/routes/reflections.router.js
@@ -22,4 +22,19 @@ const {
     });
   });
 
+  // POST route for new reflection
+  router.post("/", rejectUnauthenticated, (req, res) => {
+    const queryText = `
+    INSERT INTO "reflections" ("went_well", "needs_work", "plan_id")
+    VALUES ($1, $2, $3);
+    `;
+    pool.query(queryText, [req.body.went_well, req.body.needs_work, req.body.plan_id])
+    .then(() => {
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log("ERROR in reflections POST:", error);
+        res.sendStatus(500);
+    });
+  });
+
   module.exports = router;
