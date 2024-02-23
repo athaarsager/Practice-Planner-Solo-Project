fb0e05c4693064ea93d6926adcdd8d64e0429284
Andrew Haarsager <athaarsager@gmail.com>
2024-02-23 10:23:18 -0600

    Create local state variables and wire to form

--- src/components/ReflectionForm/ReflectionForm.jsx
+++ src/components/ReflectionForm/ReflectionForm.jsx
@@ -4,14 +4,17 @@ import { useDispatch } from "react-redux";
 
 function ReflectionForm() {
     const planId = useParams().plan_id;
+    const [wentWell, setWentWell] = useState("");
+    const [needsWork, setNeedsWork] = useState("");
+
     return (
         <>
         <p>Welcome to the Reflection Form for plan: {planId}!</p>
         <form>
             <label htmlFor="went-well">What went well this session?</label><br />
-            <input id="went-well" name="went-well" type="text" placeholder="Your Answer Here!" /><br />
+            <input id="went-well" name="went-well" type="text" placeholder="Your Answer Here!" value={wentWell} onChange={(e) => setWentWell(e.target.value)} /><br />
             <label htmlFor="needs_work">What still needs work?</label><br />
-            <input id="needs_work" name="needs_work" type="text" placeholder="Your Answer Here!" /><br />
+            <input id="needs_work" name="needs_work" type="text" placeholder="Your Answer Here!" value={needsWork} onChange={(e) => setNeedsWork(e.target.value)} /><br />
             {/* <button>Export to New Practice Session</button> */}
             <button>Save Reflection!</button>
         </form>
