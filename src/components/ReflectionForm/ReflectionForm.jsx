4610d193060b3354ab833dc534095b9c646158c4
Andrew Haarsager <athaarsager@gmail.com>
2024-02-23 10:28:25 -0600

    Submit reflection function works!

--- src/components/ReflectionForm/ReflectionForm.jsx
+++ src/components/ReflectionForm/ReflectionForm.jsx
@@ -3,14 +3,27 @@ import { useParams, useHistory } from "react-router-dom/cjs/react-router-dom.min
 import { useDispatch } from "react-redux";
 
 function ReflectionForm() {
+    const dispatch = useDispatch();
+    const history = useHistory();
     const planId = useParams().plan_id;
     const [wentWell, setWentWell] = useState("");
     const [needsWork, setNeedsWork] = useState("");
 
+    const submitReflection = (e) => {
+        e.preventDefault();
+        const payload = {
+            plan_id: planId,
+            went_well: wentWell,
+            needs_work: needsWork 
+        }
+        dispatch({ type: "ADD_REFLECTION", payload });
+        history.goBack();
+    }
+
     return (
         <>
         <p>Welcome to the Reflection Form for plan: {planId}!</p>
-        <form>
+        <form onSubmit={submitReflection}>
             <label htmlFor="went-well">What went well this session?</label><br />
             <input id="went-well" name="went-well" type="text" placeholder="Your Answer Here!" value={wentWell} onChange={(e) => setWentWell(e.target.value)} /><br />
             <label htmlFor="needs_work">What still needs work?</label><br />
