791df571bb2086ec2c7e8b936aebdce2646fb2ee
Andrew Haarsager <athaarsager@gmail.com>
2024-02-23 08:49:45 -0600

    Create getSelectedReflection saga

--- src/redux/sagas/reflections.saga.js
+++ src/redux/sagas/reflections.saga.js
@@ -1,8 +1,27 @@
 import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
-function* reflectionsSaga() {
+function* getSelectedReflection(action) {
+    // payload needs to be reflection id
+    try {
+        const getSelectedReflectionResponse = yield axios.get(`/api/reflections/${action.payload}`);
+        yield put({ type: "SET_SELECTED_REFLECTION", payload: getSelectedReflectionResponse.data});
+    } catch (error) {
+        console.error("ERROR in getSelectedReflectin saga:", error);
+    }
+}
 
+// should make get saga first...
+function* addReflection() {
+    try {
+
+    } catch (error) {
+        console.error("ERROR in addReflection saga:", error);
+    }
+}
+
+function* reflectionsSaga() {
+    yield takeLatest("GET_SELECTED_REFLECTION", getSelectedReflection);
 }
 
 export default reflectionsSaga;
