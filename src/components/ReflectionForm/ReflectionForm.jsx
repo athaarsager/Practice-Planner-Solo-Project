a97672145a78b402300f86fb7f5588bca5f8c5b4
Andrew Haarsager <athaarsager@gmail.com>
2024-02-23 10:19:49 -0600

    Create ReflectionForm html

--- src/components/ReflectionForm/ReflectionForm.jsx
+++ src/components/ReflectionForm/ReflectionForm.jsx
@@ -7,6 +7,14 @@ function ReflectionForm() {
     return (
         <>
         <p>Welcome to the Reflection Form for plan: {planId}!</p>
+        <form>
+            <label htmlFor="went-well">What went well this session?</label><br />
+            <input id="went-well" name="went-well" type="text" placeholder="Your Answer Here!" /><br />
+            <label htmlFor="needs_work">What still needs work?</label><br />
+            <input id="needs_work" name="needs_work" type="text" placeholder="Your Answer Here!" /><br />
+            {/* <button>Export to New Practice Session</button> */}
+            <button>Save Reflection!</button>
+        </form>
         </>
     );
 }
