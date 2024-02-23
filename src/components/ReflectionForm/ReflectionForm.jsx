2aebcfd3eff42cd2e236bb59850321a8c50e7ff9
Andrew Haarsager <athaarsager@gmail.com>
2024-02-23 13:47:36 -0600

    Add back button to ReflectionForm.jsx

--- src/components/ReflectionForm/ReflectionForm.jsx
+++ src/components/ReflectionForm/ReflectionForm.jsx
@@ -46,6 +46,7 @@ function ReflectionForm() {
                 <input id="went-well" name="went-well" type="text" placeholder="Your Answer Here!" value={wentWell} onChange={(e) => setWentWell(e.target.value)} /><br />
                 <label htmlFor="needs_work">What still needs work?</label><br />
                 <input id="needs_work" name="needs_work" type="text" placeholder="Your Answer Here!" value={needsWork} onChange={(e) => setNeedsWork(e.target.value)} /><br />
+                <button type="button" onClick={() => history.goBack()}>Back</button>
                 {/* <button>Export to New Practice Session</button> */}
                 { Object.keys(selectedReflection).length === 0 ?
                 <button>Save Reflection!</button> :
