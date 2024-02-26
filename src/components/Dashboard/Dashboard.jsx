8ad750ef7989e7f4252469a5354134d483538bd1
Andrew Haarsager <athaarsager@gmail.com>
2024-02-26 09:29:35 -0600

    Add Delete confirmation to dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import DashboardFooter from "../DashboardFooter/DashboardFooter";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import AddPieceDialog from "../AddPieceDialog/AddPieceDialog";
+import Swal from "sweetalert2";
 
 function Dashboard() {
     const dispatch = useDispatch();
@@ -20,7 +21,25 @@ function Dashboard() {
     }
 
     const deletePiece = (e) => {
-        dispatch({type: "DELETE_PIECE", payload: e.target.dataset.pieceid});
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                dispatch({ type: "DELETE_PIECE", payload: e.target.dataset.pieceid });
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Piece deleted.",
+                    icon: "success"
+                });
+            }
+        });
+
     }
 
     useEffect(() => {
@@ -32,11 +51,11 @@ function Dashboard() {
             <h1>Welcome to the Dashboard, {username}!</h1>
             {pieces.map(piece => (
                 <>
-                <div className="piece-icon" key={piece.id} data-pieceid={piece.id} data-piecetitle={piece.title} onClick={goToPiecePlans}>
-                    <p data-pieceid={piece.id} data-piecetitle={piece.title}>{piece.title}</p>
-                    <p data-pieceid={piece.id} data-piecetitle={piece.title}>{piece.composer}</p>
-                </div>
-                <button data-pieceid={piece.id} onClick={deletePiece}>Delete Piece</button><br />
+                    <div className="piece-icon" key={piece.id} data-pieceid={piece.id} data-piecetitle={piece.title} onClick={goToPiecePlans}>
+                        <p data-pieceid={piece.id} data-piecetitle={piece.title}>{piece.title}</p>
+                        <p data-pieceid={piece.id} data-piecetitle={piece.title}>{piece.composer}</p>
+                    </div>
+                    <button data-pieceid={piece.id} onClick={deletePiece}>Delete Piece</button><br />
                 </>
             ))}
             <button onClick={() => setAddPieceIsOpen(true)}>Add a New Piece Here!</button>
