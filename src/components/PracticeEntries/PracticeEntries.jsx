376a9305f948b5e93dc4c52155ac93344ba4668f
Andrew Haarsager <athaarsager@gmail.com>
2024-02-27 19:44:24 -0600

    Apply MUI components to PracticeEntries page

--- src/components/PracticeEntries/PracticeEntries.jsx
+++ src/components/PracticeEntries/PracticeEntries.jsx
@@ -2,6 +2,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { useEffect } from "react";
 import PracticeEntriesFooter from "./PracticeEntriesFooter";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 
 function PracticeEntries() {
@@ -12,7 +16,7 @@ function PracticeEntries() {
     const pieceId = useParams().id;
 
     const goToEditPage = (e) => {
-        dispatch({ type: "GET_SELECTED_REFLECTION", payload: e.target.dataset.planid});
+        dispatch({ type: "GET_SELECTED_REFLECTION", payload: e.target.dataset.planid });
         history.push(`/${pieceId}/practice_entries/write_reflection/${e.target.dataset.planid}`);
     }
 
@@ -22,30 +26,30 @@ function PracticeEntries() {
     }, []);
 
     return (
-        <>
-            <h2>Your Practice Plans for {selectedPiece.title}</h2>
-            <button onClick={() => history.push(`/${pieceId}/practice_entries/new_plan`)}>Add a New Practice Plan!</button>
+        <Box display="flex" flexDirection="column" alignItems="center">
+            <Typography sx={{ mb: 2 }} variant="h4">Your Practice Plans for {selectedPiece.title}</Typography>
+            <Button sx={{ mb: 2 }} onClick={() => history.push(`/${pieceId}/practice_entries/new_plan`)}>Add a New Practice Plan!</Button>
             {plans.map(plan => (
-                <div key={plan.id}>
-                    <h4>Prior Plan</h4>
-                    <p><strong>What section are you working on?</strong></p>
-                    <p>{plan.section}</p>
-                    <p><strong>What are the problems you need to solve/issues you need to address in this section?</strong></p>
-                    <p>{plan.problems}</p>
-                    <p><strong>How will you solve these problems/adderss these issues?</strong></p>
-                    <p>{plan.plan}</p>
-                    <p><strong>What is your goal for the end of the practice session?</strong></p>
-                    <p>{plan.goal}</p>
-                    <button data-planid={plan.id} onClick={(e) => history.push(`/${pieceId}/practice_entries/review_plan/${e.target.dataset.planid}`)}>Review Full Plan</button>
+                <Paper sx={{ padding: 5, mb: 2 }} key={plan.id}>
+                    <Typography sx={{ mb: 2 }} variant="h6">Prior Plan</Typography>
+                    <Typography variant="body1"><strong>What section are you working on?</strong></Typography>
+                    <Typography sx={{ mb: 1 }} variant="body2">{plan.section}</Typography>
+                    <Typography variant="body1"><strong>What are the problems you need to solve/issues you need to address in this section?</strong></Typography>
+                    <Typography sx={{ mb: 1 }} variant="body2">{plan.problems}</Typography>
+                    <Typography variant="body1"><strong>How will you solve these problems/adderss these issues?</strong></Typography>
+                    <Typography sx={{ mb: 1 }} variant="body2">{plan.plan}</Typography>
+                    <Typography variant="body1"><strong>What is your goal for the end of the practice session?</strong></Typography>
+                    <Typography sx={{ mb: 1 }} variant="body2">{plan.goal}</Typography>
+                    <Button color="secondary" data-planid={plan.id} onClick={(e) => history.push(`/${pieceId}/practice_entries/review_plan/${e.target.dataset.planid}`)}>Review Full Plan</Button>
                     {plan.reflection_written ?
-                        <button data-planid={plan.id} onClick={goToEditPage}>Edit Reflection</button> :
-                        <button data-planid={plan.id} onClick={(e) => history.push(`/${pieceId}/practice_entries/write_reflection/${e.target.dataset.planid}`)}>Write Refelction</button>
+                        <Button data-planid={plan.id} onClick={goToEditPage}>Edit Reflection</Button> :
+                        <Button data-planid={plan.id} onClick={(e) => history.push(`/${pieceId}/practice_entries/write_reflection/${e.target.dataset.planid}`)}>Write Refelction</Button>
                     }
 
-                </div>
+                </Paper>
             ))}
             <PracticeEntriesFooter />
-        </>
+        </Box>
     );
 }
 
