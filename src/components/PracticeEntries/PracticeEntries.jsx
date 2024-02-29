f8d3b7bd226f1c02653c2fc6b5d619a3da69a118
Andrew Haarsager <athaarsager@gmail.com>
2024-02-29 11:15:22 -0600

    Add breakpoints to PracticeEntries.jsx

--- src/components/PracticeEntries/PracticeEntries.jsx
+++ src/components/PracticeEntries/PracticeEntries.jsx
@@ -5,6 +5,7 @@ import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 
 function PracticeEntries() {
@@ -20,7 +21,7 @@ function PracticeEntries() {
     }
 
     const toNewReflection = (e) => {
-        dispatch({ type: "CLEAR_SELECTED_REFLECTION"});
+        dispatch({ type: "CLEAR_SELECTED_REFLECTION" });
         history.push(`/${pieceId}/practice_entries/write_reflection/${e.target.dataset.planid}`)
     }
 
@@ -43,36 +44,39 @@ function PracticeEntries() {
     useEffect(() => {
         console.log("This is the selected piece:", selectedPiece);
         dispatch({ type: "FETCH_PLANS", payload: pieceId });
-        if (Object.keys(selectedPiece).length === 0)
-        {
+        if (Object.keys(selectedPiece).length === 0) {
             dispatch({ type: "FETCH_SINGLE_PIECE", payload: pieceId });
         }
     }, []);
 
     return (
-        <Box display="flex" flexDirection="column" alignItems="center">
-            <Typography sx={{ mb: 2 }} variant="h4">Your Practice Plans for {selectedPiece.title}</Typography>
-            <Button variant="outlined" sx={{ mb: 2 }} onClick={() => history.push(`/${pieceId}/practice_entries/new_plan`)}>Add a New Practice Plan!</Button>
-            {plans.map(plan => (
-                <Paper sx={{ padding: 5, mb: 2 }} key={plan.id}>
-                    <Typography sx={{ mb: 2 }} variant="h6">Prior Plan</Typography>
-                    <Typography variant="body1"><strong>What section are you working on?</strong></Typography>
-                    <Typography sx={{ ...textLimitStyle, mb: 1 }} variant="body2">{truncateText(plan.section)}</Typography>
-                    <Typography variant="body1"><strong>What are the problems you need to solve/issues you need to address in this section?</strong></Typography>
-                    <Typography sx={{ ...textLimitStyle, mb: 1 }} variant="body2">{truncateText(plan.problems)}</Typography>
-                    <Typography variant="body1"><strong>How will you solve these problems/adderss these issues?</strong></Typography>
-                    <Typography sx={{ ...textLimitStyle, mb: 1 }} variant="body2">{truncateText(plan.plan)}</Typography>
-                    <Typography variant="body1"><strong>What is your goal for the end of the practice session?</strong></Typography>
-                    <Typography sx={{ ...textLimitStyle, mb: 1 }} variant="body2">{truncateText(plan.goal)}</Typography>
-                    <Button color="secondary" data-planid={plan.id} onClick={(e) => history.push(`/${pieceId}/practice_entries/review_plan/${e.target.dataset.planid}`)}>Review Full Plan</Button>
-                    {plan.reflection_written ?
-                        <Button data-planid={plan.id} onClick={goToEditPage}>Edit Reflection</Button> :
-                        <Button data-planid={plan.id} onClick={toNewReflection}>Write Refelction</Button>
-                    }
+        <Grid container display="flex" flexDirection="column" alignItems="center">
+            <Grid item xs={10}>
+                <Box display="flex" flexDirection="column" alignItems="center">
+                    <Typography sx={{ mb: 2 }} variant="h4">Your Practice Plans for {selectedPiece.title}</Typography>
+                    <Button variant="outlined" sx={{ mb: 2 }} onClick={() => history.push(`/${pieceId}/practice_entries/new_plan`)}>Add a New Practice Plan!</Button>
+                    {plans.map(plan => (
+                        <Paper sx={{ padding: 5, mb: 2 }} key={plan.id}>
+                            <Typography sx={{ mb: 2 }} variant="h6">Prior Plan</Typography>
+                            <Typography variant="body1"><strong>What section are you working on?</strong></Typography>
+                            <Typography sx={{ ...textLimitStyle, mb: 1 }} variant="body2">{truncateText(plan.section)}</Typography>
+                            <Typography variant="body1"><strong>What are the problems you need to solve/issues you need to address in this section?</strong></Typography>
+                            <Typography sx={{ ...textLimitStyle, mb: 1 }} variant="body2">{truncateText(plan.problems)}</Typography>
+                            <Typography variant="body1"><strong>How will you solve these problems/adderss these issues?</strong></Typography>
+                            <Typography sx={{ ...textLimitStyle, mb: 1 }} variant="body2">{truncateText(plan.plan)}</Typography>
+                            <Typography variant="body1"><strong>What is your goal for the end of the practice session?</strong></Typography>
+                            <Typography sx={{ ...textLimitStyle, mb: 1 }} variant="body2">{truncateText(plan.goal)}</Typography>
+                            <Button color="secondary" data-planid={plan.id} onClick={(e) => history.push(`/${pieceId}/practice_entries/review_plan/${e.target.dataset.planid}`)}>Review Full Plan</Button>
+                            {plan.reflection_written ?
+                                <Button data-planid={plan.id} onClick={goToEditPage}>Edit Reflection</Button> :
+                                <Button data-planid={plan.id} onClick={toNewReflection}>Write Refelction</Button>
+                            }
 
-                </Paper>
-            ))}
-        </Box>
+                        </Paper>
+                    ))}
+                </Box>
+            </Grid>
+        </Grid>
     );
 }
 
