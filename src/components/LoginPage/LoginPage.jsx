2f455a23ed81cff8524c1ea600130d66ccca2957
Andrew Haarsager <athaarsager@gmail.com>
2024-02-29 10:39:42 -0600

    Add styling to buttons on login page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
+import Button from "@mui/material/Button"
+import Box from "@mui/material/Box";
 
 function LoginPage() {
   const history = useHistory();
@@ -10,7 +12,7 @@ function LoginPage() {
       <LoginForm />
 
       <center>
-        <button
+        <Button variant="outlined" color="success"
           type="button"
           className="btn btn_asLink"
           onClick={() => {
@@ -18,7 +20,7 @@ function LoginPage() {
           }}
         >
           Register
-        </button>
+        </Button>
       </center>
     </div>
   );
